ff222220d32350be19d0595e0fbedd361eb61fd7
jaamee-s <anticamarajame@gmail.com>
2026-03-26 15:50:52 +0800

    compenent

---
+++ src/components/ExploreContainer.tsx
@@ -0,0 +1,44 @@
+import { IonButton, IonAlert } from '@ionic/react';
+import { useState } from 'react';
+import './ExploreContainer.css';
+
+interface ContainerProps { }
+
+const ExploreContainer: React.FC<ContainerProps> = () => {
+  const [showAlert, setShowAlert] = useState(false);
+
+  const alertButtons = [
+    {
+      text: 'Cancel',
+      role: 'cancel',
+      handler: () => {
+        setShowAlert(false);
+      },
+    },
+    {
+      text: 'OK',
+      role: 'confirm',
+      handler: () => {
+        setShowAlert(false);
+      },
+    },
+  ];
+
+  return (
+    <div id="container">
+      <IonButton id="present-alert" onClick={() => setShowAlert(true)}>
+        Click Me
+      </IonButton>
+      <IonAlert
+        isOpen={showAlert}
+        onDidDismiss={() => setShowAlert(false)}
+        header="A Short Title Is Best"
+        subHeader="A Sub Header Is Optional"
+        message="A message should be a short, complete sentence."
+        buttons={alertButtons}
+      />
+    </div>
+  );
+};
+
+export default ExploreContainer;
